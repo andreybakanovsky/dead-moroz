fbac26de8aa85265d9d32b408fe01867fd982df7
andrey bakanovsky <andrey.bakanovsky@outlook.com>
2021-12-01 22:55:54 +0300

    Create the Goods resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
+  has_many :goods, dependent: :destroy
   validates :role, presence: true
   validates :name, presence: true, length: { minimum: 2, maximum: 20 }
-
   enum role: { kid: 0, elf: 1, dead_moroz: 2 }
 end
