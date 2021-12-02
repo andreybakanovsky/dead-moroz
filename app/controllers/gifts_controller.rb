c19fa8b828bdbff3f81a0ce02d8793eba6e1e960
andrey bakanovsky <andrey.bakanovsky@outlook.com>
2021-12-02 15:22:21 +0300

    Del before_action from the Gifts controller

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,17 +1,10 @@
 class GiftsController < ApplicationController
-  before_action :set_gift, only: %i[show edit update destroy]
 
   def index
     @gifts = Gift.all
   end
 
   def show; end
-
-  def new
-    @gift = Gift.new
-  end
-
-  def edit; end
 
   def create
     @gift = Gift.new(gift_params)
@@ -24,24 +17,25 @@ class GiftsController < ApplicationController
   end
 
   def update
-    if @gift.update(gift_params)
-      redirect_to @gift, notice: 'Gift was successfully updated.'
+    if gift.update(gift_params)
+      redirect_to gift, notice: 'Gift was successfully updated.'
     else
       render :edit
     end
   end
 
   def destroy
-    @gift.destroy
+    gift.destroy
     redirect_to gifts_url, notice: 'Gift was successfully destroyed.'
   end
 
   private
 
   # Use callbacks to share common setup or constraints between actions.
-  def set_gift
-    @gift = Gift.find(params[:id])
+  def gift
+    @gift ||= Gift.find(params[:id])
   end
+  helper_method :gift
 
   def gift_params
     params.require(:gift).permit(:name, :description, :deads_choice)
