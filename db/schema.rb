e58a144e84a9380a3f77047eb4e47daf051d1033
andrey bakanovsky <andrey.bakanovsky@outlook.com>
2021-12-12 13:45:48 +0300

    Change a field name for the Invitations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_09_165909) do
+ActiveRecord::Schema.define(version: 2021_12_12_081118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2021_12_09_165909) do
   create_table "invitations", force: :cascade do |t|
     t.string "email"
     t.date "expire_at"
-    t.text "utl"
+    t.text "url"
     t.integer "status"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
