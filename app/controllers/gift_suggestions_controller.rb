89cd0807d61caedbec0412fcc6da05790bec0d79
andrey bakanovsky <andrey.bakanovsky@outlook.com>
2021-12-26 23:54:12 +0300

    Add GiftSuggestions controller

---
+++ app/controllers/gift_suggestions_controller.rb
@@ -0,0 +1,67 @@
+class GiftSuggestionsController < ApiController
+  load_and_authorize_resource :gift, param_method: :gift_suggestions_params # +
+
+  def index
+    gifts = review.suggested_gifts.accessible_by(current_ability)
+    render json: gifts
+  end
+
+  def show
+    # @review = Review.accessible_by(current_ability,:show)
+    # @gift = review.suggested_gifts.find(params[:id])
+    @gift ||= review.suggested_gifts.accessible_by(current_ability).find(params[:id])
+    render json: gift
+  end
+
+  def create
+    gift = review.suggested_gifts.build(gift_suggestions_params)
+    if gift.save
+      render json: gift, status: :created
+    else
+      render json: gift.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if gift.update(gift_suggestions_params)
+      render json: gift
+    else
+      render json: gift.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if gift.destroy
+      head :no_content, status: :ok
+    else
+      render json: gift.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user
+    @user ||= User.find(params[:user_id])
+  end
+
+  def good
+    @good ||= user.goods.find(params[:good_id])
+  end
+
+  def review
+    @review ||= good.reviews.find(params[:review_id])
+  end
+
+  def gifts
+    @gifts ||= review.suggested_gifts
+  end
+
+  def gift
+    # @gift ||= Gift.find(params[:id])
+    @gift ||= review.suggested_gifts.find(params[:id])
+  end
+
+  def gift_suggestions_params
+    params.require(:gift_suggestion).permit(:name, :description, :giftable_type, :giftable_id)
+  end
+end
