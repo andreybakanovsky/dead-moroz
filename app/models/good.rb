0fe81fdadc12b251f3118a7808ce3794e31427d0
andrey bakanovsky <andrey.bakanovsky@outlook.com>
2021-12-28 21:46:37 +0300

    Update models

--- app/models/good.rb
+++ app/models/good.rb
@@ -1,8 +1,8 @@
 class Good < ApplicationRecord
   belongs_to :child, class_name: 'User', foreign_key: 'user_id', inverse_of: :goods
   has_many :reviews, dependent: :destroy
-  has_many :requested_gifts, -> { where(giftable_type: 'Good') }, as: :giftable, class_name: 'Gift',
-            dependent: :destroy, inverse_of: :giftable
+  has_many :requested_gifts, as: :giftable, class_name: 'Gift', dependent: :destroy
+
   validates :year, presence: true,
          numericality: { greater_than_or_equal_to: 2000, less_than_or_equal_to: proc { |_record| Date.current.year } }
   validates :content, length: { maximum: 1000 }
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :good
   belongs_to :author, class_name: 'User', foreign_key: 'user_id', inverse_of: :reviews
-  has_many :suggested_gifts, -> { where(giftable_type: 'Review') }, as: :giftable, class_name: 'Gift',
-             dependent: :destroy, inverse_of: :giftable
+  has_many :suggested_gifts, as: :giftable, class_name: 'Gift', dependent: :destroy
+
   validates :grade, presence: true,
              numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
   validates :comment, length: { maximum: 1000 }
