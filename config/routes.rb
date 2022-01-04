ce86a436dc45f044ee1acb725aa2b04c94fda13e
andrey bakanovsky <andrey.bakanovsky@outlook.com>
2022-01-04 13:03:31 +0300

    Fix space in routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,16 @@ Rails.application.routes.draw do
   scope :api, defaults: { format: :json } do
     scope :v1 do
       scope :authentication do
-      devise_scope :user do
-      post   '/sign_in',        to: 'devise/sessions#create'
-      delete '/sign_out',       to: 'devise/sessions#destroy'
-      post   '/sign_up',        to: 'devise/registrations#create'
-      put    '/account_update', to: 'devise/registrations#update'
-      delete '/account_delete', to: 'devise/registrations#destroy'
+        devise_scope :user do
+          post   '/sign_in',        to: 'devise/sessions#create'
+          delete '/sign_out',       to: 'devise/sessions#destroy'
+          post   '/sign_up',        to: 'devise/registrations#create'
+          put    '/account_update', to: 'devise/registrations#update'
+          delete '/account_delete', to: 'devise/registrations#destroy'
+        end
       end
     end
   end
-end
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
