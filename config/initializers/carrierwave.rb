b4d6a5fd799577b6fb799b798e0bfc4f29f724db
andrey bakanovsky <andrey.bakanovsky@outlook.com>
2022-03-23 11:28:01 +0300

    Add rails app credentials for AWS

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],     # required unless using use_iam_profile
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # required unless using use_iam_profile
-    region:                ENV['AWS_REGION'],            # optional, defaults to 'us-east-1'
+    provider:              'AWS',                                                 # required
+    aws_access_key_id:     Rails.application.credentials.aws[:access_key_id],     # required unless using use_iam_profile
+    aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key], # required unless using use_iam_profile
+    region:                Rails.application.credentials.aws[:region],            # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = ENV['AWS_BUCKET']                                     # required
+  config.fog_directory  = Rails.application.credentials.aws[:bucket]            # required
   config.fog_public     = false                                                 # optional, defaults to true
   config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
   # For an application which utilizes multiple servers but does not need caches persisted across requests,
