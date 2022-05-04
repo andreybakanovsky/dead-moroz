b53599de902b66d1e323f042c28523af8c6dbad4
andrey bakanovsky <andrey.bakanovsky@outlook.com>
2022-05-04 13:36:25 +0300

    Add the order in the query

--- app/queries/approved_gifts.rb
+++ app/queries/approved_gifts.rb
@@ -4,19 +4,19 @@ class ApprovedGifts < BaseQuery
       Review
       .select('reviews.id')
       .joins("INNER JOIN gifts ON gifts.giftable_type = 'Review' AND reviews.id = gifts.giftable_id")
-      .where(gifts: { deads_choice: true })
       .joins('INNER JOIN users ON reviews.user_id = users.id')
+      .where(gifts: { deads_choice: true })
       .where(users: { id: @params[:user_id] })
 
     Gift
       .select('users.id AS users_id, users.name AS users_name')
       .select('goods.year')
       .select('gifts.id, gifts.name AS gifts_name, gifts.description, gifts.images')
-      .where(gifts: { deads_choice: true })
       .joins("INNER JOIN reviews ON gifts.giftable_type = 'Review' AND reviews.id = gifts.giftable_id")
-      .where(reviews: { id: reviews_approved_gifts })
       .joins('INNER JOIN goods ON reviews.good_id = goods.id')
       .joins('INNER JOIN users ON goods.user_id = users.id')
+      .where(gifts: { deads_choice: true })
+      .where(reviews: { id: reviews_approved_gifts })
 
     # in this case we can't get gifts images url by mount_uploaders:
     # User
