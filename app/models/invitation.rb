8727a266f34236316ecf176f3f85604751781d46
andrey bakanovsky <andrey.bakanovsky@outlook.com>
2022-05-17 15:11:40 +0300

    Create the email validation

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -7,21 +7,34 @@ class Invitation < ApplicationRecord
 
   validates :status, presence: true, inclusion: { in: Invitation.statuses }
   validates :email, presence: true
+  validate :email, :email_cannot_used_if_taken_by_users
+
+  before_save :downcase_email
 
   def create_token
     self.token = SecureRandom.uuid # v4
     save_digest
   end
 
-  def create_digest
-    BCrypt::Password.create(token)
-  end
-
   def expire_date
     EXPIRING_PERIOD.from_now
   end
 
+  private
+
   def save_digest
     update(digest: create_digest)
+  end
+
+  def email_cannot_used_if_taken_by_users
+    errors.add(:email, 'the email address has already been taken') if User.find_by(email: email.downcase).present?
+  end
+
+  def create_digest
+    BCrypt::Password.create(token)
+  end
+
+  def downcase_email
+    self.email = email.downcase
   end
 end
