5359d06a9f1d2975d1407dca4a7c66d1729594ba
andrey bakanovsky <andrey.bakanovsky@outlook.com>
2022-06-01 22:19:10 +0300

    Add the searching results highlighting

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -7,12 +7,27 @@ module Api
                     []
                   else
                     Elasticsearch::Model
-                      .search(params[:query], models)
+                      .search(params[:query], models, { body: highlight_fields })
                       .results.as_json
                       .group_by { |result| result['_index'] }
 
                   end
         render json: results
+      end
+
+      private
+
+      def highlight_fields
+        {
+          highlight: {
+            fields: {
+              '*': {
+                pre_tags: ['<em>'],
+              post_tags: ['</em>']
+              }
+            }
+          }
+        }
       end
     end
   end
