3810a20f6f4c3cb85b125cece5c42441862b5e41
andrey bakanovsky <andrey.bakanovsky@outlook.com>
2022-06-03 17:29:07 +0300

    Add searching through all models
    It's only Dead moroz ability

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -3,23 +3,28 @@ module Api
     class SearchController < ApplicationController
       def search
         authorize! :search_smth, :search
-        models = [Gift]
+        case current_user.role.to_sym
+        when :dead_moroz
+          models = []
+        when :elf
+          models = [Gift]
+        end
         results = if params[:query].blank?
                     []
                   else
                     Elasticsearch::Model
-                      .search(params[:query], models, { body: highlight_fields })
+                      .search(params[:query], models, { body: search_params })
                       .results.as_json
-                      .group_by { |result| result['_index'] }
-
+                    # .group_by { |result| result['_index'] }
                   end
         render json: results
       end
 
       private
 
-      def highlight_fields
+      def search_params
         {
+          size: 20,
           highlight: {
             fields: {
               '*': {
